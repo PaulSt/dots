09f49f6824c1260c17542c6b040d7c10c10412f7
Paul <stocker.paul@hotmail.com>
2026-05-04 15:54:55 +0200

    pertag patch and font sizes

--- local/dwm/config.h
+++ local/dwm/config.h
@@ -7,7 +7,7 @@ static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=8"};
+static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#282828";
